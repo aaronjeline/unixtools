0f4a2ea37ac149a21bed05195ca7328714461de2
Aaron Eline <aaroneline.github@gmail.com>
2017-06-16 11:53:29 -0400

    File size

--- ls.c
+++ ls.c
@@ -9,25 +9,38 @@
 
 typedef struct flags_{
 	bool dotFiles;
+	bool rawSize;
 }flags;
+
 
 void handleArgs(char* arg, flags* flag){
 	for(int i=1;i<strlen(arg);i++){
 		switch(arg[i]){
 			case 'a': flag->dotFiles = true;break;
+			case 'l': flag->rawSize = true;break;
+			default: printf("Unknown arg %c passed\n",arg[i]);break;
 		}
 	}
 	return;
 }
 
+//Initialize the flags struct, set all default values
 flags* initFlags(){
 	flags* def = malloc(sizeof(flags));
 	if(def!=NULL){
 		def->dotFiles = false;
+		def->rawSize = false;
 	}
 	return def;
 }
 
+int getFileSize(struct dirent* file){
+	FILE* stream = fopen(file->d_name, "rb");
+	fseek(stream,0L,SEEK_END);
+	int size = ftell(stream);
+	fclose(stream);
+	return size;
+}
 
 int main(int argc, char* argv[]){
 	flags* flags = initFlags();
@@ -60,6 +73,15 @@ int main(int argc, char* argv[]){
 					case DT_REG: printf(KWHT);break;
 				}
 				printf(cur->d_name);
+				//Size
+				if(flags->rawSize){
+					printf(KNRM);
+					printf(": ");
+					switch(cur->d_type){
+						case DT_DIR: printf(" directory");break;
+						default: printf(": %d bytes",getFileSize(cur));
+					}
+				}
 				printf("\n");
 			}
 		}
